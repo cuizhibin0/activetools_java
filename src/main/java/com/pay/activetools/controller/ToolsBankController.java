5404463db2e6499a135dea5e90d6db646f50040d
cuizhibin0 <18518679659@163.com>
2018-08-23 23:11:14 +0800

    银行表查询

--- src/main/java/com/pay/activetools/controller/ToolsBankController.java
+++ src/main/java/com/pay/activetools/controller/ToolsBankController.java
@@ -2,15 +2,19 @@ package com.pay.activetools.controller;
 
 
 import com.alibaba.fastjson.JSONObject;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.pay.activetools.bean.JsonResultView;
 import com.pay.activetools.entity.ToolsBank;
 import com.pay.activetools.enums.CodeEnum;
 import com.pay.activetools.service.ToolsBankService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -29,16 +33,26 @@ public class ToolsBankController {
     @Autowired
     private ToolsBankService toolsBankService;
 
-    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
     @ResponseBody
-    public JSONObject saveToolsBank(ToolsBank toolsBank){
+    public JSONObject saveToolsBank(ToolsBank toolsBank) {
         try {
             toolsBankService.insert(toolsBank);
         } catch (Exception e) {
-            logger.error("保存:{}异常:{}",e);
+            logger.error("保存:{}异常:{}", e);
             return CodeEnum.FAIL.getJsonMsg();
         }
         return CodeEnum.SUCCESS.getJsonMsg();
+
+    }
+
+    public JsonResultView findToolsBank(ToolsBank toolsBank, @RequestParam(value = "num", defaultValue = "1") Integer num,
+                                        @RequestParam(value = "size", defaultValue = "10") Integer size) {
+        JsonResultView view = new JsonResultView();
+        Page<ToolsBank> page = new Page<ToolsBank>(num,size);
+        Page<ToolsBank> toolsBankPage = toolsBankService.selectPage(page);
+
+        return view;
 
     }
 
